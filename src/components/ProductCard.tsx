60d4333d0e0dae405f7cfe842e8dae31bffad278
Mouse1Mouse <misha.taracuk@gmail.com>
2025-06-10 02:28:57 +0300

    Fix JSX syntax errors in ProductCard component

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -282,7 +282,7 @@ export default function ProductCard({ item }: ProductCardProps) {
                     />
                   ))}
                 </div>
-              )}
+              </>
             )}
           </div>
           
